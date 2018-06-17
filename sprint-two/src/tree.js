3500712ef0a48ed194194994df6ccaaa13ec8be6
Monsoon <nepzsoon@gmail.com>
2018-06-17 13:07:54 -0700

    Finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,32 @@
 var Tree = function(value) {
   var newTree = {};
+
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var output = false;
+
+  //basecase
+  if (this.value === target) {
+    output = true;
+  }
+  //recursive case
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      output = output || this.children[i].contains(target);
+    }
+  }
+  return output;
 };
 
 
@@ -21,3 +34,4 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
