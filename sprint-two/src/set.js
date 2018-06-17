60e228b6da3e98560a641ebec38fc924879eb83c
Monsoon <nepzsoon@gmail.com>
2018-06-17 13:08:40 -0700

    Finished Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.includes(item)) {
+  }
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  var value = this._storage;
+  if (value.includes(item)) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if (index > -1) {
+    this._storage.splice(index, 1);
+  }
 };
+
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
