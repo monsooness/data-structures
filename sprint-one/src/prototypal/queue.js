d1f64eb9b74c1e842960a5129236ae2c418c8d86
Monsoon <nepzsoon@gmail.com>
2018-06-17 13:02:16 -0700

    Finished prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.first = 0;
+  newInstance.last = 0;
+
+  return newInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
 
+queueMethods.dequeue = function() {
+  var output = this[this.first];
+  this.first++;
+  return output;
+};
+
+queueMethods.size = function() {
+  var size = this.last - this.first;
+  return size > 0 ? size : 0;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(stackMethods);
+  newInstance.first = 0;
+  newInstance.last = 0;
+
+  return newInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this[this.last] = value;
+  this.last++;
+};
+
+stackMethods.pop = function() {
+  var output = this[this.last - 1];
+  this.last--;
+  return output;
+};
+
+stackMethods.size = function() {
+  var size = this.last - this.first;
+  return size > 0 ? size : 0;
+};
 
 
