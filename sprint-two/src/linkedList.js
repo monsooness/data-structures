d633356d2e901eb8dc51528fd100d64d127eb041
Monsoon <nepzsoon@gmail.com>
2018-06-17 13:06:57 -0700

    Finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (this.tail === null) {
+      this.tail = node;
+      this.head = node;
+    } else {
+      this.tail = node;
+      this.head.next = this.tail;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head !== this.tail) {
+      var headoriginal = this.head.value;
+      var pointed = this.head.next;
+      this.head = pointed;
+      return headoriginal;
+    }
+    return this.tail.value; 
   };
 
+
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }  
+    }
+    return false;
   };
 
   return list;
@@ -17,7 +39,6 @@ var LinkedList = function() {
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
 
