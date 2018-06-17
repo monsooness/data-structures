0a5e2fd70380848679651069ffdc1096bd3cba26
Monsoon <nepzsoon@gmail.com>
2018-06-17 13:05:34 -0700

    Finished ES6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.first = 0;
+    this.last = 0;
+  }
+
+  enqueue(value) {
+    this[this.last] = value;
+    this.last++;
+  }
+
+  dequeue() {
+    var output = this[this.first];
+    this.first++;
+    return output;
+  }
+
+  size() {
+    var size = this.last - this.first;
+    return size > 0 ? size : 0;
   }
 
 }
+
+var newInstance = new Queue();
+
+
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.first = 0;
+    this.last = 0;
+  }
+  
+  push(value) {
+    this[this.last] = value;
+    this.last++;
   }
 
+  pop() {
+    var output = this[this.last-1];
+    this.last--;
+    return output;
+  }
+
+  size() {
+    var size = this.last - this.first;
+    return size > 0 ? size : 0;
+  }
 }
+
+var newInstance = new Stack();
