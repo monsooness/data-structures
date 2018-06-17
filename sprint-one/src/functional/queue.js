784d72c4dd3bfd4167ed25272ff4badef15e7392
Monsoon <nepzsoon@gmail.com>
2018-06-17 12:58:56 -0700

    Finished functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var first = 0;
+  var last = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,19 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    var output = storage[first];
+    first++;
+    return output;
   };
 
   someInstance.size = function() {
+    var size = last - first;
+    return size > 0 ? size : 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var first = 0;
+  var last = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.pop = function() {
+    var output = storage[last-1];
+    last--;
+    return output;
   };
 
   someInstance.size = function() {
+    var size = last - first;
+    return size > 0 ? size : 0;
   };
 
   return someInstance;
 };
+
+
